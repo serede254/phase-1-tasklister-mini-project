cfb80414d2cd8b9b4fd43b3ef7aa5483e991c363
root <root@SURFACE.>
2024-03-27 10:57:00 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+  
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+
+    
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(newTask);
+    
+})
+})
