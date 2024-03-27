5435e7c85daa2ef78d337f3441ef5384164037e0
root <root@SURFACE.>
2024-03-27 11:35:16 +0300

    uu

--- src/index.js
+++ src/index.js
@@ -12,9 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const newTask = document.createElement("li");
     newTask.textContent = taskDescription;
 
-    
     const taskList = document.getElementById("tasks");
     taskList.appendChild(newTask);
-    
+
+    sortedTasksArray = taskList.sort(function(a, b) {
+      return b.dataset.priority - a.dataset.priority;})
+
+ var deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+   newTask.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function() {
+            taskList.removeChild(newTask);
+})
 })
 })
